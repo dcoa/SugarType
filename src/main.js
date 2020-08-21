4d16a9543a3ea4917d8e4d4a203354fdcfad56d8
MarGo-20 <d.marcelagonzalez@gmail.com>
2020-08-21 16:48:18 -0500

    Ajustes rutas

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
-     
-changePage = function () {window.location.href = 'micontrol2.html}
+
+const formulario = document.querySelector("#formulario");
+formulario.addEventListener("submit", (e) => {e.preventDefault();
+    return window.location.href = './Views/micontrol2.html';
+});
+
+const buttonGlucometrias = document.querySelector("#buttonGlucometrias");
+buttonGlucometrias.addEventListener("click", () => {
+    window.location.href = './Views/glucometrias3.html';
+});
+
+const buttonMedicamentos = document.querySelector("#buttonMedicamentos");
+buttonMedicamentos.addEventListener("click", () => {
+    window.location.href = './Views/medicamentos5.html';
+});
+
+const buttonExamenes = document.querySelector("#buttonExamenes");
+buttonExamenes.addEventListener("click", () => {
+    window.location.href = './Views/examenes6.html';
+});
+
+const buttonDoctor = document.querySelector("#buttonDoctor");
+buttonDoctor.addEventListener("click", () => {
+    window.location.href = './Views/doctor8.html';
+});
+
