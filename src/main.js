1c5c2052b1eb1b7f2c150549e1721850dbe20ce9
MarGo-20 <d.marcelagonzalez@gmail.com>
2020-08-21 20:31:54 -0500

    Registro e inicio de sesión

--- src/main.js
+++ src/main.js
@@ -1,26 +1,68 @@
+//Inicio sesion
+const formLogin = document.querySelector("#formLogin");
+if(formLogin)
+formLogin.addEventListener("submit", (e) => {e.preventDefault();
 
-const formulario = document.querySelector("#formulario");
-formulario.addEventListener("submit", (e) => {e.preventDefault();
-    return window.location.href = './Views/micontrol2.html';
+    const correoInicio = document.querySelector("#correoInicio").value;
+    const contrasenaInicio = document.querySelector("#contrasenaInicio").value;
+    const mensajeInicio = document.querySelector("#mensajeInicio");
+
+    console.log(correoInicio, contrasenaInicio);
+
+    auth.signInWithEmailAndPassword(correoInicio, contrasenaInicio)
+    .then(userCredential => { 
+        console.log("Ingreso"); 
+            return window.location.href = './Views/micontrol2.html';
+    })
+    .catch (err => {
+        console.log(err);
+        if (err.code === "auth/user-not-found"){
+                mensajeInicio.innerHTML = "Usuario no registrado, por favor regístrese";
+        }
+        if(err.code === "auth/wrong-password"){
+                mensajeInicio.innerHTML = "Contraseña incorrecta";
+        }
+    })
+
 });
 
-const buttonGlucometrias = document.querySelector("#buttonGlucometrias");
-buttonGlucometrias.addEventListener("click", () => {
-    window.location.href = './Views/glucometrias3.html';
+//Registrar usuario 
+const formRegistry = document.querySelector("#formRegistry");
+if(formRegistry)
+formRegistry.addEventListener("submit", (e) => {e.preventDefault();
+    const correoRegistro = document.querySelector("#correoRegistro").value;
+    const contrasenaRegistro = document.querySelector("#contrasenaRegistro").value;
+    const mensajeRegistro = document.querySelector("#mensajeRegistro");
+
+    console.log(correoRegistro, contrasenaRegistro);
+
+    auth.createUserWithEmailAndPassword(correoRegistro, contrasenaRegistro)
+    .then(userCredential => { 
+        console.log("Registro"); 
+            return window.location.href = './Views/micontrol2.html';
+    })
+    .catch (err => {
+        console.log("Usuario registrado");
+        mensajeRegistro.innerHTML = "Usuario ya registrado, inicie sesión";
+    })
+    
 });
 
-const buttonMedicamentos = document.querySelector("#buttonMedicamentos");
-buttonMedicamentos.addEventListener("click", () => {
-    window.location.href = './Views/medicamentos5.html';
+const linkRegistar = document.querySelector("#linkRegistar");
+if(linkRegistar)
+linkRegistar.addEventListener("click", (e) => {e.preventDefault();
+    const containerRegistry = document.querySelector("#containerRegistry");
+    const containerLogIn = document.querySelector("#containerLogIn");
+    containerRegistry.style.display= "block";
+    containerLogIn.style.display= "none";
 });
 
-const buttonExamenes = document.querySelector("#buttonExamenes");
-buttonExamenes.addEventListener("click", () => {
-    window.location.href = './Views/examenes6.html';
-});
-
-const buttonDoctor = document.querySelector("#buttonDoctor");
-buttonDoctor.addEventListener("click", () => {
-    window.location.href = './Views/doctor8.html';
+const linkInicio = document.querySelector("#linkInicio");
+if(linkInicio)
+linkInicio.addEventListener("click", (e) => {e.preventDefault();
+    const containerRegistry = document.querySelector("#containerRegistry");
+    const containerLogIn = document.querySelector("#containerLogIn");
+    containerRegistry.style.display= "none";
+    containerLogIn.style.display= "block";
 });
 
